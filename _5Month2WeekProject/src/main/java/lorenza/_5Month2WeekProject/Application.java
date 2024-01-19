9a8a9bd280344bec723243578144d5d85151a6d9
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-01-19 11:43:28 +0100

    Initial commit

---
+++ _5Month2WeekProject/src/main/java/lorenza/_5Month2WeekProject/Application.java
@@ -0,0 +1,13 @@
+package lorenza._5Month2WeekProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ _5Month2WeekProject/src/test/java/lorenza/_5Month2WeekProject/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lorenza._5Month2WeekProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
