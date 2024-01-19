2841180fb04a3e32129e5a9cb2a8ff256a3f69f7
D'Amore Lorenza <lorenzadamore@outlook.it>
2024-01-19 15:03:42 +0100

    3

--- _5Month2WeekProject/src/main/java/lorenza/_5Month2WeekProject/controllers/UsersController.java
+++ _5Month2WeekProject/src/main/java/lorenza/_5Month2WeekProject/controllers/UsersController.java
@@ -4,10 +4,10 @@ import lorenza._5Month2WeekProject.entities.Users;
 import lorenza._5Month2WeekProject.services.UserService;
 import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -18,6 +18,28 @@ public class UsersController {
     @GetMapping
     public Page<Users> getUsers(@RequestParam int page, @RequestParam int size,@RequestParam String orderBy) {
         return userService.getUsers(page, size, orderBy);
+    }
+
+    @GetMapping("/{userUUID}")
+    public Users getUserByUUID(@PathVariable UUID userUUID) {
+        return userService.findById(userUUID);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Users createUser(@RequestBody Users newUserPayload) {
+        return userService.save(newUserPayload);
+    }
+
+    @PutMapping("/{userUUID}")
+    public Users getUserByUUIDAndUpdate(@PathVariable UUID userUUID, @RequestBody Users modifiedUserPayload) {
+        return userService.findByIdAndUpdate(userUUID, modifiedUserPayload);
+    }
+
+    @DeleteMapping("/{userUUID}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void getUserByUUIDAndDelete(@PathVariable UUID userUUID) {
+        userService.findByIdAndDelete(userUUID);
     }
 
 }
